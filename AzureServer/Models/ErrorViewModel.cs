c1b0a0f6164970a704ebeb03ac78a2e1b1297728
Azimjon <azimjonshodiyorov@gmail.com>
2024-02-05 23:00:56 +0500

    Add project files.

---
+++ AzureServer/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
